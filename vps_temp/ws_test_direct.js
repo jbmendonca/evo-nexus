cdf8c2a225108ce2e48f9c4d17603f95a548065f
João Batista da Silva Mendonça <joao.mendoncarr@gmail.com>
2026-05-14 20:36:29 -0400

    chore: remove vps_temp from tracking (contains exposed secret)

--- vps_temp/ws_test_direct.js
+++
@@ -1,7 +0,0 @@
-const WebSocket = require("ws");
-const ws = new WebSocket("ws://127.0.0.1:32352/ws");
-ws.on("open", () => { console.log("WS_CONNECTED"); ws.send(JSON.stringify({type:"ping"})); });
-ws.on("message", d => { console.log("WS_MSG:" + d.toString()); ws.close(); });
-ws.on("error", e => { console.log("WS_ERR:" + e.message); process.exit(1); });
-ws.on("close", (c,r) => { console.log("WS_CLOSE_CODE=" + c); process.exit(0); });
-setTimeout(() => { console.log("WS_TIMEOUT"); process.exit(1); }, 5000);
--- vps_temp/ws_test_internal.js
+++
@@ -1,7 +0,0 @@
-const WebSocket = require("ws");
-const ws = new WebSocket("ws://127.0.0.1:32352/terminal/ws");
-ws.on("open", () => { console.log("CONNECTED_OK"); ws.send(JSON.stringify({type:"ping"})); });
-ws.on("message", d => console.log("MSG:", d.toString()));
-ws.on("error", e => console.log("ERR:", e.message));
-ws.on("close", (c,r) => { console.log("CLOSE code=" + c); process.exit(0); });
-setTimeout(() => { ws.close(); process.exit(0); }, 5000);
